f3b3b835a398196b567b2300a78b2a7e92a29c3e
Borislav Grigorov <b.s.grigorov@gmail.com>
2024-10-29 11:43:26 -0600

    icp onchain svc app

--- servers/icp-onchain/src/app.ts
+++ servers/icp-onchain/src/app.ts
@@ -1,6 +1,56 @@
-import { App } from './index';
-import { ICPRoute } from './routes/icp.route';
 
-const app = new App([new ICPRoute()]);
+import 'reflect-metadata';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from './config';
+import { Routes } from './interfaces/routes.interface';
 
-app.listen();
+export class App {
+  public app: express.Application;
+  public env: string;
+  public port: string | number;
+
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.env = NODE_ENV || 'development';
+    this.port = PORT || 3000;
+
+    this.initializeRoutes(routes);
+    this.initializeMiddlewares();
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.info(`=================================`);
+      console.info(`======= ENV: ${this.env} =======`);
+      console.info(`🚀 App listening on the port ${this.port}`);
+      console.info(`=================================`);
+    });
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(morgan(LOG_FORMAT));
+    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(hpp());
+    this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+  }
+
+  private initializeRoutes(routes: Routes[]) {
+    routes.forEach(route => {
+      this.app.use('/', route.router);
+    });
+  }
+}
