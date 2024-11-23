c7f04e525ef875d903204a529cd670de606db4b3
Boyan Klisurov <boyanklisurov@gmail.com>
2024-11-23 20:04:57 +0200

    ICPT-93 SNS&NNS examples update

---
+++ examples/src/sns/treasury.ts
@@ -0,0 +1,9 @@
+import { Principal } from "@dfinity/principal";
+import { AccountManager as SNSAccountManager } from "@icp-tokens-node/nns-integration-package"; 
+
+const governanceTreasuryBalance = {
+  icp: await SNSAccountManager.getIcpTreasuryBalance(Principal.fromText("2jvtu-yqaaa-aaaaq-aaama-cai")),
+  icrc_token: await SNSAccountManager.getIcrcTokenTreasuryBalance(Principal.fromText("2jvtu-yqaaa-aaaaq-aaama-cai"), Principal.fromText("2ouva-viaaa-aaaaq-aaamq-cai"))
+};
+
+console.log(governanceTreasuryBalance);
